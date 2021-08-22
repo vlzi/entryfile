11e8f741c51e3fcbd0d9a7aeeb54e18fc433de1d
vlzi <76029766+vlzi@users.noreply.github.com>
2021-08-22 23:45:11 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-(function()
+(async function()
 {
-    if (~location.href.indexOf("new")) return "저장하시고 다시 실행해주세요.";
+    if (~location.href.indexOf("ws/new")) return "새 프로젝트입니다. 저장하시고 다시 실행해주세요.";
+    if (!confirm("코드를 불러오는 과정에서 저장하시지 않은 정보는 지워질 수 있습니다. 그러나 전에 코드를 불러오신 후 사용하시고 저장하신 후에 다시 코드를 불러오시지 않은 상태에서 저장하시면 저장된 코드가 지워집니다. 코드를 불러오시겠습니까?")) return "불러오기를 중지합니다.";
+    if (Entry.engine.isState('run')) await Entry.engine.stopButton.click();
     let files = [];
     Entry.block.ask_files = {
         color: "#dd47d8",
@@ -21,13 +23,13 @@
             input.type = "file"; input.multiple = true;
             input.onchange = async () => {
                 files = await Promise.all(Array.from(input.files).map(
-                    (file) => new Promise(async(res, rej) => {
+                    (file) => new Promise((res, rej) => {
                         const reader = new FileReader();
-                        reader.onload = event => { res(Array.from(new Int8Array(event.target.result)).map(b=>b+128)); }
+                        reader.onload = event => { res({name: file.name, bytes: Array.from(new Int8Array(event.target.result)).map(b=>b+128)}); }
                         reader.readAsArrayBuffer(file);
                     })
                 ));
-                console.log(files, 345);
+                console.log(files, 567);
             };
             input.click();
         }
@@ -40,8 +42,20 @@
         Entry.playground.blockMenu._createThread(t);
     });
     Entry.playground.blockMenu.code.changeEvent.notify();
-    const project = Entry.exportProject();
+    const project = (await fetch("/graphql",
+    {
+        headers: { "Content-Type": "application/json" },
+        body : JSON.stringify({
+            query: `query($id: ID! $groupId: ID) { project(id: $id, groupId: $groupId) {
+                objects scenes variables messages functions tables
+                speed interface expansionBlocks aiUtilizeBlocks learning 
+            } }`,
+            variables: { id: "60abba8e0eb8ee022a85871b" }
+        }),
+        method: "POST"
+    }).then(r=>r.json())).data.project;
     Entry.clearProject();
     Entry.loadProject(project);
     Entry.playground.showBlockMenu();
-})();
+})().then(()=>console.log("완료되었습니다."));
+undefined;
